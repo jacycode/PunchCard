ca4a25d15f0cf00e350864f77005a5fc2c472b01
刘清 <liuqing@marrymin.com>
2018-07-25 16:07:55 +0800

    增加高德定位插件  提供位置服务

--- index.js
+++ index.js
@@ -1,3 +1,65 @@
+//W3C标准API
+if(navigator.geolocation) {
+    console.log('start w3C');
+    navigator.geolocation.getCurrentPosition(
+        function (position) {
+            console.log('sss');
+            var longitude = position.coords.longitude;
+            var latitude = position.coords.latitude;
+            console.log(longitude)
+            console.log(latitude)
+        },
+        function (e) {
+            console.log('eee');
+            var msg = e.code;
+            var dd = e.message;
+            console.log(msg)
+            console.log(dd)
+        },
+        {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+        }
+    )
+}
+
+//高德插件API
+var latitude;
+var longtitude;
+var amap = new AMap.Map('iCenter');
+amap.plugin('AMap.Geolocation', function() {
+    console.log('start AMap');
+    var geolocation = new AMap.Geolocation({
+        // 是否使用高精度定位，默认：true
+        enableHighAccuracy: true,
+        // 设置定位超时时间，默认：无穷大
+        timeout: 10000,
+        // 定位按钮的停靠位置的偏移量，默认：Pixel(10, 20)
+        buttonOffset: new AMap.Pixel(10, 20),
+        //  定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
+        zoomToAccuracy: true,
+        //  定位按钮的排放位置,  RB表示右下
+        buttonPosition: 'RB'
+    })
+
+    geolocation.getCurrentPosition()
+    AMap.event.addListener(geolocation, 'complete', onComplete)
+    AMap.event.addListener(geolocation, 'error', onError)
+
+    function onComplete (data) {
+        // data是具体的定位信息
+        console.log(data);
+        latitude = data.position.getLat();
+        longtitude = data.position.getLng();
+    }
+
+    function onError (data) {
+        // 定位出错
+        console.log(data);
+    }
+})
+
 //请求打卡数据
 function loadrefresh() {
     if($.cookie('username') == undefined){
@@ -30,49 +92,32 @@ loadrefresh();
 
 //打卡按钮 点击事件
 $("#punchBt").on('click', function () {
-    if(navigator.geolocation) {
-        console.log('获取位置');
-        navigator.geolocation.getCurrentPosition(
-            function (position) {
-                console.log('成功');
-                var longitude = position.coords.longitude;
-                var latitude = position.coords.latitude;
-                console.log(longitude)
-                console.log(latitude)
-            },
-            function (e) {
-                console.log('失败');
-                var msg = e.code;
-                var dd = e.message;
-                console.log(msg)
-                console.log(dd)
-            },
-            {
-                enableHighAccuracy: true,
-                timeout: 5000,
-                maximumAge: 0
-            }
-        )
-    }
-    //打卡
-    $.ajax("/punch", {
-        method:"post",
-        data: {"username": $.cookie('username'),
-               "punchtime": new Date().getTime()},
-        success: function (data, code, xhr) {
-            if (code == 'success'){
-                if(data.code == 0){
-                    loadrefresh();
+    if(longtitude == undefined || latitude == undefined){
+        alert('locate...');
+    }else if(latitude > 39.946 && latitude < 39.947 && longtitude > 116.292 && longtitude < 116.293){
+        //打卡
+        $.ajax("/punch", {
+            method:"post",
+            data: {"username": $.cookie('username'),
+                "punchtime": new Date().getTime()},
+            success: function (data, code, xhr) {
+                if (code == 'success'){
+                    if(data.code == 0){
+                        loadrefresh();
+                    }else{
+                        alert('server error');
+                    }
                 }else{
-                    alert('server error');
+                    alert('net error');
                 }
-            }else{
+            },
+            error: function () {
                 alert('net error');
-            }
-        },
-        error: function () {
-            alert('net error');
-        },
-        dataType:"json"
-    });
+            },
+            dataType:"json"
+        });
+    }else{
+        alert("invalid area");
+    }
+
 });
